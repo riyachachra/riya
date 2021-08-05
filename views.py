cddb598c9f146941cccda19c4a224a46ceff6b4e
riyachachra <88493619+riyachachra@users.noreply.github.com>
2021-08-05 18:21:30 +0530

    Delete views.py

--- views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from djuser.models import Users
-
-def Showuser(request):
-    resultsdisplay = Users.objects.all()
-    return render(request, "Page1.html", {'Users': resultsdisplay})
